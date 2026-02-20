8dd9e679767149ed0d5f7070c6cf821d50223a48
c4rl0s04 <carlosandreshuete@gmail.com>
2026-02-21 00:23:08 +0100

    refactor: rename middleware.js to proxy.js.

--- src/proxy.js
+++ src/proxy.js
@@ -3,6 +3,6 @@ import { auth } from './auth';
 export default auth;
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico|login).*)'],
 };
