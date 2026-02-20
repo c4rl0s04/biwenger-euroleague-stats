f24fe3bdcb9aaa1cb995af006d4c2946f49c3bce
c4rl0s04 <carlosandreshuete@gmail.com>
2026-02-21 00:32:13 +0100

    feat: Remove the SnowfallEffect component and its `react-snowfall` dependency.

--- src/components/layout/ClientWrapper.js
+++ src/components/layout/ClientWrapper.js
@@ -4,14 +4,12 @@ import { UserProvider } from '@/contexts/UserContext';
 import { UserSelectionModal } from '@/components/user';
 import ThemeBackground from '@/components/ui/ThemeBackground';
 import ErrorBoundary from '@/components/ui/ErrorBoundary';
-import SnowfallEffect from '@/components/ui/SnowfallEffect';
 import CommandPalette from '@/components/ui/CommandPalette';
 
 export default function ClientWrapper({ children, users }) {
   return (
     <UserProvider users={users}>
       <ThemeBackground />
-      <SnowfallEffect />
       <CommandPalette />
       <UserSelectionModal />
       <ErrorBoundary>{children}</ErrorBoundary>
